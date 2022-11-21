08510ea2aca226cbc555ad8d3c020f483ff501c9
stillssi <109871454+Stillssi@users.noreply.github.com>
2022-11-21 11:18:06 +0900

    디렉터리 생성 코드 수정

--- data_save/extract_pdf_text.py
+++ data_save/extract_pdf_text.py
@@ -3,15 +3,22 @@ import shutil
 from tika import parser
 
 def extract_pdf_text():
-    PDF_DIR = '/Users/stillssi/Desktop/BOK_TEAM_4/pdf_data' #PDF 파일 경로
+    PDF_DIR = '/Users/stillssi/Desktop/BOK_TEAM_4/Data/pdf_data' #PDF 파일 경로
     os.listdir(PDF_DIR)
 
     file_list = os.listdir(PDF_DIR)
-
     for file in file_list:    
-        pdf_path =  f"/Users/stillssi/Desktop/BOK_TEAM_4/pdf_data/{file}" 
+        pdf_path =  f"/Users/stillssi/Desktop/BOK_TEAM_4/Data/pdf_data/{file}" 
         parsed = parser.from_file(pdf_path)
-        SAVE_DIR = '/Users/stillssi/Desktop/BOK_TEAM_4/text_data/'+file[0:10]+'.txt'
+        
+        SAVE_FOLDER = '/Users/stillssi/Desktop/BOK_TEAM_4/Data/text_data/'
+        if not os.path.isdir(SAVE_FOLDER):
+                os.mkdir(SAVE_FOLDER)
+        
+        SAVE_DIR = '/Users/stillssi/Desktop/BOK_TEAM_4/Data/text_data/'+file[0:10]+'.txt'
         txt = open(SAVE_DIR, 'w', encoding = 'utf-8')
         print(parsed['content'], file = txt)
         txt.close()
+
+
+extract_pdf_text()

aa27635fb6a9473c1eb00731797659eee258791b
stillssi <109871454+Stillssi@users.noreply.github.com>
2022-11-24 09:50:29 +0900

    의사록 의결 날짜로 파일 이름 수정

--- 01_Crawling/minutes/data_save/text_spliting.py
+++ 01_Crawling/minutes/data_save/text_spliting.py
@@ -1,24 +1,45 @@
 import os
 import shutil
+import re
 def split_text():
-    TXT_DIR = '/Users/stillssi/Desktop/BOK_TEAM_4/Data/text_data/'
+    TXT_DIR = '/Users/stillssi/Desktop/minutes/Data/text_data/'
     file_list = os.listdir(TXT_DIR)
+    
     for file in file_list:
         f = open(TXT_DIR+file, 'r', encoding='utf-8')
         lines = f.readlines()
         lines = list(map(lambda s: s.strip(), lines))
+        #의결 날짜 추출
+        date_index = [i for i in range(len(lines)) if '일 자' in lines[i] or '일    자' in lines[i] or '일 시' in lines[i] or '일    시' in lines[i]]
+        date = re.sub(r'[^0-9]', '', lines[date_index[0]])[1:]
+        year = date[0:4]
+        if len(date) == 6:
+            month = date[4]
+            day = date[5]
+        elif len(date) == 7:
+            month = date[4]
+            day = date[5:]
+        else:
+            month = date[4:6]
+            day = date[6:]
+
+        
+        #필요 내용 추출
         first_index = [i for i in range(len(lines)) if '의결안건' in lines[i]]
         second_index = [i for i in range(len(lines)) if '위원 토의내용' in lines[i]]
         last_index = [j for j in range(len(lines)) if '심의결과' in lines[j]]
-        print(first_index, second_index, last_index)
-        SAVE_FOLDER = '/Users/stillssi/Desktop/BOK_TEAM_4/Data/split_data/'
+
+        SAVE_FOLDER = '/Users/stillssi/Desktop/minutes/Data/split_data/'
         if not os.path.isdir(SAVE_FOLDER):
             os.mkdir(SAVE_FOLDER)
-        k = open('/Users/stillssi/Desktop/BOK_TEAM_4/Data/split_data/'+file, 'a', encoding='utf-8')
+        #의결 날짜로 파일명
+        k = open('/Users/stillssi/Desktop/minutes/Data/split_data/'+f'{year}.{month}.{day}.txt', 'a', encoding='utf-8')
+        
         try:
             k.write(''.join(lines[first_index[0]:second_index[0]]))
         except:
             pass
+        
         for i,j in zip(second_index, last_index):
             k.write(''.join(lines[i:j]))
         k.close()
--- 02_Preprocessing/minutes_preprocessing/minutes_ngram.py
+++ 02_Preprocessing/minutes_preprocessing/minutes_ngram.py
@@ -2,22 +2,25 @@ import re
 import os
 from eKoNLPy.ekonlpy.sentiment import MPCK
 import itertools
-
+from eKoNLPy.ekonlpy.tag import MeCab
 def minutes_ngram():
     '''
     의사록 5-gram 함수
     return: n-gram 개수
     '''
     ngram_list = []
-
+    mpck = MPCK()
+    mecab = MeCab()
     TXT_DIR = '//Users/stillssi/Desktop/minutes/Data/split_data/' #split text 파일 경로
     file_list = os.listdir(TXT_DIR)
     for file in file_list:
         f = open(TXT_DIR+file,'r', encoding='utf-8')
+        
         lines = f.readlines()
 
         hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')
         text = []
+        
         for l in lines:
             result = hangul.sub('', l)
             result = result.split(' ')
@@ -27,13 +30,15 @@ def minutes_ngram():
         text = list(itertools.chain(*text))
         resultt = ' '.join(text)   
 
-        mpck = MPCK()
         tokens = mpck.tokenize(resultt)
+        
+        mpck.get_ngram(tokens=tokens, )
         ngrams = mpck.ngramize(tokens)
-        ngram_list.append(ngrams)
-        score = mpck.classify(tokens + ngrams, intensity_cutoff=1.5)
-        # ngram_list = list(itertools.chain(*ngram_list))
-        # ngram_list = list(set(ngram_list))
-    return len(ngram_list)
+        
+        score = mpck.classify(tokens + ngrams, intensity_cutoff=1.3)
+        ngram_list_1 = list(itertools.chain(*ngram_list))
+        ngram_list_1 = list(set(ngram_list_1))
+
+    return len(ngram_list_1)
 
 minutes_ngram()

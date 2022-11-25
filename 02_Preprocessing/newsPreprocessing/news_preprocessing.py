2c4f4f55a6eebabe21664ed5e76cfcac8f567aae
Bowon Choi <102291110+Choi-Bowon@users.noreply.github.com>
2022-11-25 09:31:25 +0900

    Add if __add__ code

--- 02_Preprocessing/newsPreprocessing/news_preprocessing.py
+++ 02_Preprocessing/newsPreprocessing/news_preprocessing.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 from ekonlpy.sentiment import MPCK
 import itertools
+
 def cleansing(text):
     repl =''
     pattern = '([ㄱ-ㅎㅏ-ㅣ]+)' # 자음, 모음 제거
@@ -18,6 +19,7 @@ def cleansing(text):
     pattern = '\.([^\.]*(?:기자|뉴스|특파원|교수|작가|대표|논설|고문|주필|부문장|팀장|장관|원장|연구원|이사장|위원|실장|차장|부장|에세이|화백|사설|소장|단장|과장|기획자|큐레이터|저작권|평론가|©|©|ⓒ|\@|\/|=|:앞쪽_화살표:|무단|전재|재배포|금지|\[|\]|\(\))[^\.]*)$'
     text = re.sub(pattern = pattern, repl=repl,string=text)
     return text
+
 def load_data_preprocessing():
     df=pd.read_csv('./naver_news_scrapy_total_prep.csv', usecols=['media','date','content'])
     # 분석할 기간에 해당하는 데이터 추출
@@ -31,6 +33,7 @@ def load_data_preprocessing():
     # 본문 데이터 정제
     df['content'] = df['content'].map(lambda x: cleansing(x))
     return df.to_csv('naver_news_prep.csv', index=False)
+
 def visualization(): # 언론사, 연도 별 뉴스량 시각화
     df=pd.read_csv('./naver_news_prep.csv')
     # date 컬럼 데이터 타입 datetime으로 변경
@@ -50,6 +53,7 @@ def visualization(): # 언론사, 연도 별 뉴스량 시각화
     plt.xticks(index, fontsize=7, rotation=90)
     plt.figure(figsize=(25,22))
     plt.show()
+
 def news_ngram_list(text): # ngram 생성
     ngram_list=[]
     mpck = MPCK()
@@ -58,11 +62,13 @@ def news_ngram_list(text): # ngram 생성
     ngram_list.append(ngrams)
     ngram_list = list(itertools.chain(*ngram_list)) # 이중 리스트 형성 안되도록 풀어주기
     return ngram_list
+
 def news_ngram(): # ngram 추출 후 new_df에 저장
     new_df=pd.DataFrame(columns=['date','ngrams'])
     df=pd.read_csv('./naver_news_prep.csv',usecols=['date','content'])
     new_df['date']=df['date'].map(lambda x: pd.to_datetime(x))
     new_df['ngrams'] = df['content'].map(lambda x: news_ngram_list(x))
     return new_df
-df=news_ngram()
-df.to_csv('news_ngram_data.csv')
+
+if __name__ == "__main__":
+    df=news_ngram().to_csv('news_ngram_data.csv')
